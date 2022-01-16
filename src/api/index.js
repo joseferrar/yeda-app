fdc9fb0331b51b121950d6c9147115b76f6160b1
joseferrar <joseferrar4@gmail.com>
2022-01-16 18:33:06 +0530

    delete edm product api files

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,3 @@ import axios from "axios";
 export const REACT_URL = axios.create({
   baseURL: "https://yeda-backend.herokuapp.com",
 });
-
-export const FOOD_API = axios.create({
-  baseURL: `https://api.edamam.com`,
-});
--- src/services/FoodService.js
+++
@@ -1,7 +0,0 @@
-import { FOOD_API } from "../api";
-import { api_id, api_key } from "../api/key";
-export const getFood = () =>
-  FOOD_API.get(`/search?q=biryani&app_id=${api_id}&app_key=${api_key}`);
-
-export const getSearch = (query) =>
-  FOOD_API.get(`/search?q=${query}&app_id=${api_id}&app_key=${api_key}`);
